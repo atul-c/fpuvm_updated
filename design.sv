5d5cb47ed32256b8baff05825f7484e76ee8c3bf
atul-c <93982211+atul-c@users.noreply.github.com>
2022-05-05 16:10:39 +0530

    Memory arbiter verilog
    run this on edaplayground and check results

--- design.sv
+++ design.sv
@@ -1,45 +1,136 @@
-// Code your design here
-module fixed_arb (input [2:0] req, output reg [2:0] grant);
-  
-  always @ (req)
- // case (1'b1) : 
-    if (req[0])
-      grant = 3'b001;
-  else if (req[1])
-       grant = 3'b010;
-  else if (req[2])
-      grant = 3'b100;
-  //end
-  //endcase
-  
-endmodule
+// FSM Design Verilog
+// DMA/TDSP Bus Arbiter
+// Asynchronous reset, active high
+//
 
+module arb_v (
+	reset,
+	clk,
+	dma_breq,
+	dma_grant,
+	tdsp_breq,
+	tdsp_grant
+	);
+  
+  input
+  	reset,
+  	clk,
+  	dma_breq,
+  	tdsp_breq;
+  
+  output reg
+  	dma_grant,
+  	tdsp_grant;
 
-module round_robin_arbiter (input clk,rst,[2:0] req, output [2:0] grant);
   
-  logic [2:0] local_grant,raw_grant;
-  logic [2:0] mask_req ;
-  logic [2:0] mask;
+  // include state encodings defined in arb.h
+  `include "arb.h"
   
-  always @ (posedge clk or negedge rst) begin
-    if (!rst)
-      mask <= 3'b111;
-    else if (grant[0])
-      mask <= 3'b110;
-    else if (grant[1])
-      mask <= 3'b100;
-    else if (grant [2])
-      mask <= 3'b000;
-    
-  end
+  // STATE REGISTERS
+  reg [2:0] state; // current state
+  reg [2:0] next_state;
   
+  always@(posedge clk or posedge reset)
+    begin
+      if(reset)
+        begin
+          state <= `IDLE;
+        end
+      else
+        state <= next_state;
+    end
   
-  assign mask_req = req & mask;
+  // Compute next state based on present state: combinational logic
+  always@(state or tdsp_breq or dma_breq)
+    begin
+      case (state)
+        `IDLE:
+          begin
+          	if(tdsp_breq)
+            	next_state = `GRANT_TDSP;
+          	else if(dma_breq)
+            	next_state = `GRANT_DMA;
+				else next_state = `IDLE;
+        	end
+        `GRANT_TDSP:
+          begin
+            if(tdsp_breq == 1'b0)
+              next_state = `CLEAR;
+				 else next_state = `GRANT_TDSP;
+          end
+        `GRANT_DMA:
+          begin
+            if(dma_breq == 1'b0)
+              next_state = `CLEAR;
+				else next_state = `GRANT_DMA;
+          end
+        `DMA_PRI:
+          begin
+            if(tdsp_breq)
+              next_state = `GRANT_TDSP;
+            else if(dma_breq)
+              next_state = `GRANT_DMA;
+            else if(!tdsp_breq && !dma_breq)
+              next_state = `IDLE;
+				else next_state = `DMA_PRI;
+          end
+        `CLEAR:
+          begin
+            if(tdsp_breq == 1'b1)
+              next_state = `GRANT_TDSP;
+            else if(dma_breq)
+              next_state = `DMA_PRI;
+				else next_state = `CLEAR;
+          end
+         default: next_state = `IDLE;
+      endcase
+    end
   
-  fixed_arb f1(.req(mask_req),.grant(local_grant));
-  fixed_arb f2(.req(req),.grant(raw_grant));
+  // output at clock edge based on next state: Registered output, glitch free
+  always@(posedge clk or posedge reset)
+    begin
+		if(reset)
+			begin
+				dma_grant <= 1'b0;
+				tdsp_grant <= 1'b0;
+			end
+		else
+			begin
+				case(next_state) // based on next state assign the output
+
+				  `IDLE:
+					 begin
+						dma_grant = 1'b0;
+						tdsp_grant = 1'b0;
+					 end
+				  `GRANT_TDSP:
+					 begin
+						dma_grant = 1'b0;
+						tdsp_grant = 1'b1;
+					 end
+				  `GRANT_DMA:
+					 begin
+						dma_grant = 1'b1;
+						tdsp_grant = 1'b0;
+					 end
+				  `DMA_PRI:
+					 begin
+						dma_grant = 1'b0;
+						tdsp_grant = 1'b0;
+					 end
+				  `CLEAR:
+					 begin
+						dma_grant = 1'b0;
+						tdsp_grant = 1'b1;
+					 end
+				  default:
+					 begin
+						dma_grant = 1'b0;
+						tdsp_grant = 1'b0;
+					 end
+				endcase
+		end
+        
+    end
   
-  assign grant = (mask == 3'b000) ? raw_grant : local_grant;
-  
-  
-endmodule
+ endmodule
